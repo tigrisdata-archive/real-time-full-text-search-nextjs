cd2aea0646b90535b0fabab6b878b7c66097a2d0
icode247 <zionekekenta@gmail.com>
2023-01-11 09:39:28 +0100

    demo UI completed

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+    images: {
+      domains: ['images.pexels.com','hellbunny.com','cdn.shopify.com','www.lulus.com', 'cdn-img.prettylittlething.com','avatars.githubusercontent.com', 's.gravatar.com'],
+  },
+}
+
+module.exports = nextConfig
